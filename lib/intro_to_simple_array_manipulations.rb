a128ff5e0a33a6fdb16ba07e36a730ad2b89d7e5
saman batool <samanbatool08@gmail.com>
2019-10-21 20:18:22 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,15 @@
+def artists = [ "Hozier" , "Ariana Grande" , "Usher" ]
+  next_artist = "Beyonce"
+  artists.push(next_artist)
+end
+
+p artists
+
+def artists [ "Frida Kahlo" , "Pablo Picasso" , "Car Guo-Qiang"]
+  artists.pop
+end
+
+p artists
+
+def array(array)
+  
